4da566edac9fe6927f37b078fc2bfd487dc89a3f
Denis78-sys <denismarques736gmail.com>
2024-02-06 10:44:00 -0300

    7º commit, criando o componente menu

---
+++ components/molecules/Menu.vue
@@ -0,0 +1,26 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+    <div class="menu">
+        <nav>
+            <ul class="flex space-x-20">
+                <li><NuxtLink to="/">Home</NuxtLink></li>
+                <li><NuxtLink to="/contato">Contato</NuxtLink></li>
+            </ul>
+
+        </nav>
+    </div>
+</template>
+
+<script>
+export default {
+    setup () {
+        
+
+        return {}
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,7 @@
 <template>
     <div>
         <Logo />
+        <Menu />
     </div>
 </template>
 
